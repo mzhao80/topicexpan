b3e3871a718650c814e3507f89a11fed2e49021b
Michael Zhao <michaelzhao@college.harvard.edu>
2025-01-15 19:33:43 -0500

    Update train_job_congress.sh

--- train_job_congress.sh
+++ train_job_congress.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:8                # Request 4 GPUs
 #SBATCH --cpus-per-task=8           # Request 8 CPUs
 #SBATCH --mem=20G                   # Request 20 GB of memory
-#SBATCH --time=1-00:00:00           # Set the maximum runtime (1 day)
+#SBATCH --time=0-12:00:00           # Set the maximum runtime (12 hr)
 #SBATCH --open-mode=append
 #SBATCH --output=logs/train_%j.out       # Standard output log file (with job ID)
 #SBATCH --error=logs/train_%j.err        # Standard error log file (with job ID)
